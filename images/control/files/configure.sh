56543ed64cc4b3e7228e227262b092d46047201a
Adam Heath <doogie@brainfood.com>
2018-03-15 10:41:08 -0500

    Use a multi-stage Dockerfile to fetch the docker binary, instead of extracting it via dpkg-deb.
    This reduces the download size, and since we no longer need to use
    apt-get to find the correct url, we can reduce the size of the target
    image(at the time of this commit, that's 16M).

--- images/control/files/configure.sh
+++ images/control/files/configure.sh
@@ -9,23 +9,14 @@ declare -a scripts=(
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y apt-transport-https gnupg openssl wget libltdl7 haproxy
-cp -a /tmp/files/docker-sources.list /etc/apt/sources.list.d
-apt-key add /tmp/files/docker-key.gpg 
-
-apt-get update
-
-docker_ce_url=$(apt-get install -dy --print-uris -qq docker-ce | sed -n "s/^'\\(.*docker-ce_.*\\)' .*/\\1/p")
-wget -O /tmp/docker-ce.deb "$docker_ce_url"
-
-dpkg-deb --fsys-tarfile /tmp/docker-ce.deb | tar xf - -C / ./usr/bin/docker
+apt-get install -y openssl wget libltdl7 haproxy
 
 mkdir -p /srv/localdev/scripts
 for script in "${scripts[@]}"; do
 	cp -a "/tmp/files/$script.sh" "/srv/localdev/scripts/$script"
 done
 
-rm /tmp/files /tmp/docker-ce.deb -rf
+rm /tmp/files -rf
 
 apt-get clean
 find /var/cache/apt /var/lib/apt -type f -delete
