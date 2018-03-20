59fa520797c6d0b80222e935f0fd6880f0a34b6d
Adam Heath <doogie@brainfood.com>
2018-03-20 15:44:46 -0500

    Add an nginx frontend for nexus3.

---
+++ images/nexus3/nginx-files/configure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+declare -a scripts=(
+	entrypoint
+	healthcheck
+)
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /etc/nginx/conf.d
+cp /tmp/files/nginx-docker.conf /etc/nginx/conf.d/nginx-docker.conf
+
+mkdir -p /srv/localdev/scripts
+for script in "${scripts[@]}"; do
+	cp -a "/tmp/files/$script.sh" "/srv/localdev/scripts/$script"
+done
+
+rm /tmp/files -rf
+
+apt-get clean
+find /var/cache/apt /var/lib/apt -type f -delete
