37da32a945452b148275ba35d65c58d3b50bb14e
Adam Heath <doogie@brainfood.com>
2018-03-22 12:00:23 -0500

    This is now more object oriented.

--- images/nexus3/healthcheck.py
+++ images/nexus3/healthcheck.py
@@ -9,8 +9,9 @@ import urllib
 from Cookie import SimpleCookie
 
 class API():
-    def __init__(self, address, port):
+    def __init__(self, address, port, encoder):
         self.connection = httplib.HTTPConnection(address, port)
+        self.encoder = encoder
         self.cookies = SimpleCookie()
         self.tid = 1
         self.logged_in = False
@@ -53,14 +54,160 @@ class API():
         tid = self.tid
         self.tid += 1
         payload = {'action': obj, 'method': method, 'data': data, 'type': 'rpc', 'tid': tid}
-        response = self.send('POST', '/service/extdirect', 'application/json', json.dumps(payload))
+        response = self.send('POST', '/service/extdirect', 'application/json', json.dumps(payload, cls = self.encoder))
         result_payload = json.loads(response.read().decode())
         return result_payload['result']
 
-def build_maven2_proxy_config(name, version_policy, remote_url):
-    return [{"attributes": {"maven": {"versionPolicy": version_policy, "layoutPolicy": "STRICT"}, "proxy": {"remoteUrl": remote_url, "contentMaxAge": 1440, "metadataMaxAge": 1440}, "httpclient": {"blocked": False, "autoBlock": True, "connection": {"useTrustStore": False}}, "storage": {"blobStoreName": "default", "strictContentTypeValidation": True}, "negativeCache": {"enabled": True, "timeToLive": 1440}}, "name": name, "format": "", "type": "", "url": "", "online": True, "authEnabled": False, "httpRequestSettings": False, "recipe": "maven2-proxy"}]
+class RepoAttrDocker(object):
+    key = 'docker'
 
-api = API('127.0.0.1', 8081)
+    def __init__(self, **args):
+        self.httpPort = args.get('docker_httpPort', None)
+        self.forceBasicAuth = args.get('docker_forceBasicAuth', False)
+        self.v1Enabled = args.get('docker_v1Enabled', True)
+
+class RepoAttrProxy(object):
+    key = 'proxy'
+
+    def __init__(self, **args):
+        self.remoteUrl = args['proxy_remoteUrl']
+        self.contentMaxAge = args.get('proxy_contentMaxAge', 1440)
+        self.metadataMaxAge = args.get('proxy_metadataMaxAge', 1440)
+
+class RepoAttrDockerProxy(object):
+    key = 'dockerProxy'
+
+    def __init__(self, **args):
+        self.indexType = args.get('dockerProxy_indexType', 'REGISTRY')
+
+class RepoAttrHttpClient(object):
+    key = 'httpclient'
+
+    def __init__(self, **args):
+        self.blocked = args.get('httpClient_blocked', False)
+        self.autoBlock = args.get('httpClient_autoBlock', True)
+        self.connection = {'httpClient_useTrustStore': args.get('httpClient_connection_useTrustStore', False)}
+
+class RepoAttrStorage(object):
+    key = 'storage'
+
+    def __init__(self, **args):
+        self.blobStoreName = args.get('storage_blobStoreName', 'default')
+        self.strictContentTypeValidation = args.get('storage_strictContentTypeValidation', True)
+        self.writePolicy = args.get('storage_writePolicy')
+
+class RepoAttrNegativeCache(object):
+    key = 'negativeCache'
+
+    def __init__(self, **args):
+        self.enabled = args.get('negativeCache_enabled', True)
+        self.timeToLive = args.get('negativeCache_timeToLive', 1440)
+
+class RepoAttrGroup(object):
+    key = 'group'
+
+    def __init__(self, **args):
+        self.memberNames = args['group_memberNames']
+
+def serializeOne(instance):
+    result = {}
+    attributeList = []
+    for name, value in instance.__dict__.items():
+        #print('Trying %s=%s' % (name, value))
+        if name == 'attributes':
+            attributeList = value
+            continue
+        if not value == None:
+            result[name] = value
+    result['attributes'] = attributes = {}
+    for attribute in attributeList[::-1]:
+        key = attribute.__class__.key
+        if key in attributes:
+            continue
+        attributes[key] = serializeOne(attribute)
+    return result
+
+class Repo(object):
+    def __init__(self, **args):
+        self.name = args['name']
+        self.format = args.get('format', '')
+        self.type = args.get('type', '')
+        self.url = args.get('url', '')
+        self.online = args.get('online', True)
+        self.authEnabled = args.get('authEnabled', False)
+        self.httpRequestSettings = args.get('httpRequestSettings', False)
+        self.attributes = []
+        self.attributes.append(RepoAttrStorage(**args))
+
+    def serialize(self):
+        result = serializeOne(self)
+        foo = self.__class__.__dict__
+        result['recipe'] = self.__class__.__dict__.get('recipe')
+        print(result)
+        return result
+
+class ProxyRepo(Repo):
+    def __init__(self, **args):
+        super(ProxyRepo, self).__init__(**args)
+        self.attributes.append(RepoAttrProxy(**args))
+        self.attributes.append(RepoAttrHttpClient(**args))
+        self.attributes.append(RepoAttrNegativeCache(**args))
+
+class GroupRepo(Repo):
+    def __init__(self, **args):
+        super(GroupRepo, self).__init__(**args)
+        self.attributes.append(RepoAttrGroup(**args))
+
+class DockerRepo(Repo):
+    def __init__(self, **args):
+        super(DockerRepo, self).__init__(**args)
+        self.attributes.append(RepoAttrDocker(**args))
+
+class DockerHostedRepo(DockerRepo):
+    recipe = 'docker-hosted'
+
+    def __init__(self, **args):
+        args['storage_writePolicy'] = 'ALLOW'
+        super(DockerHostedRepo, self).__init__(**args)
+
+class DockerProxyRepo(ProxyRepo, DockerRepo):
+    recipe = 'docker-proxy'
+
+    def __init__(self, **args):
+        super(DockerProxyRepo, self).__init__(**args)
+        self.attributes.append(RepoAttrDockerProxy(**args))
+
+class DockerGroupRepo(GroupRepo, DockerRepo):
+    recipe = 'docker-group'
+
+    def __init__(self, **args):
+        super(DockerGroupRepo, self).__init__(**args)
+
+class RepoAttrMaven2(object):
+    key = 'maven'
+
+    def __init__(self, **args):
+        self.versionPolicy = args['maven_versionPolicy']
+        self.layoutPolicy = args.get('maven_layoutPolicy', 'STRICT')
+
+class Maven2Repo(Repo):
+    def __init__(self, **args):
+        super(Maven2Repo, self).__init__(**args)
+        self.attributes.append(RepoAttrMaven2(**args))
+
+class Maven2ProxyRepo(ProxyRepo, Maven2Repo):
+    recipe = 'maven2-proxy'
+
+    def __init__(self, **args):
+        super(Maven2ProxyRepo, self).__init__(**args)
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Repo):
+            return o.serialize()
+        return super(CustomEncoder, self).default(o)
+
+api = API('127.0.0.1', 8081, CustomEncoder)
 
 found_repos = {}
 result = api.call('coreui_Repository', 'readReferences', [{'page': 1, 'start': 0, 'limit': 100, 'filter': [{'property': 'applyPermissions', 'value': True}]}])
@@ -69,18 +216,18 @@ for item in result['data']:
 
 autovivify_repos = []
 if not 'docker-hosted' in found_repos:
-    autovivify_repos.append([{'attributes': {'docker': {'httpPort': 8082, 'forceBasicAuth': False, 'v1Enabled': True}, 'storage': {'blobStoreName': 'default', 'strictContentTypeValidation': True, 'writePolicy': 'ALLOW'}}, 'name': 'docker-hosted', 'format': '', 'type': '', 'url': '', 'online': True, 'recipe': 'docker-hosted'}])
+    autovivify_repos.append(DockerHostedRepo(name = 'docker-hosted', docker_httpPort = 8082))
 if not 'docker-proxy' in found_repos:
-    autovivify_repos.append([{"attributes": {"docker": {"forceBasicAuth": False, "v1Enabled": True}, "proxy": {"remoteUrl": "https://registry-1.docker.io", "contentMaxAge": 1440, "metadataMaxAge": 1440}, "dockerProxy": {"indexType": "REGISTRY"}, "httpclient": {"blocked": False, "autoBlock": True, "connection": {"useTrustStore": False}}, "storage": {"blobStoreName": "default", "strictContentTypeValidation": True}, "negativeCache": {"enabled": True, "timeToLive": 1440}}, "name": "docker-proxy", "format": "", "type": "", "url": "", "online": True, "authEnabled": False, "httpRequestSettings": False, "recipe": "docker-proxy"}])
+    autovivify_repos.append(DockerProxyRepo(name = 'docker-proxy', proxy_remoteUrl = 'https://registry-1.docker.io'))
 if not 'docker-group' in found_repos:
-    autovivify_repos.append([{"attributes": {"docker": {"httpPort": 8083, "forceBasicAuth": False, "v1Enabled": True}, "storage": {"blobStoreName": "default", "strictContentTypeValidation": True}, "group": {"memberNames": ["docker-hosted", "docker-proxy"]}}, "name": "docker-group", "format": "", "type": "", "url": "", "online": True, "recipe": "docker-group"}])
+    autovivify_repos.append(DockerGroupRepo(name = 'docker-group', docker_httpPort = 8083, group_memberNames = ['docker-hosted', 'docker-proxy']))
 
 if not 'spring-milestone' in found_repos:
-    autovivify_repos.append(build_maven2_proxy_config('spring-milestone', 'RELEASE', 'https://repo.spring.io/milestone'))
+    autovivify_repos.append(Maven2ProxyRepo(name = 'spring-milestone', maven_versionPolicy = 'RELEASE', proxy_remoteUrl = 'https://repo.spring.io/milestone'))
 if not 'spring-snapshot' in found_repos:
-    autovivify_repos.append(build_maven2_proxy_config('spring-snapshot', 'SNAPSHOT', 'https://repo.spring.io/snapshot'))
+    autovivify_repos.append(Maven2ProxyRepo(name = 'spring-snapshot', maven_versionPolicy = 'SNAPSHOT', proxy_remoteUrl = 'https://repo.spring.io/snapshot'))
 if not 'rabbit-milestone' in found_repos:
-    autovivify_repos.append(build_maven2_proxy_config('rabbit-milestone', 'RELEASE', 'https://dl.bintray.com/rabbitmq/maven-milestones'))
+    autovivify_repos.append(Maven2ProxyRepo(name = 'rabbit-milestone', maven_versionPolicy = 'RELEASE', proxy_remoteUrl = 'https://dl.bintray.com/rabbitmq/maven-milestones'))
 
 if len(autovivify_repos):
     if os.path.isfile('/nexus-data/healthcheck/first-time'):
@@ -98,8 +245,8 @@ if len(autovivify_repos):
     if len(autovivify_realms):
         api.call('coreui_RealmSettings', 'update', [{'realms': found_realms + autovivify_realms}])
 
-    for data in autovivify_repos:
-        api.call('coreui_Repository', 'create', data)
+    for repo in autovivify_repos:
+        foo = api.call('coreui_Repository', 'create', [repo])
 
     result = api.call('coreui_Repository', 'read', None)
     maven_members = []
