e8243889cba2cff79622a2d881dd820093b51aef
Adam Heath <doogie@brainfood.com>
2018-03-26 09:31:59 -0500

    Don't call groupmod/usermod when there is nothing to do.

--- images/control/adjust-user.sh
+++ images/control/adjust-user.sh
@@ -11,16 +11,39 @@ TARGET_USER="$1"
 TARGET_GROUP="$2"
 shift 2
 
-target_home="$(getent passwd "$TARGET_USER" | cut -f 6 -d :)"
+if ! [[ $(getent passwd "$TARGET_USER") =~ ^([^:]+):([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):(.*)$ ]]; then
+	echo "Couldn't parse: getent passwd" 1>&2
+	exit 1
+fi
+declare -A user=(
+	[name]="${BASH_REMATCH[1]}"
+	[password]="${BASH_REMATCH[2]}"
+	[uid]="${BASH_REMATCH[3]}"
+	[gid]="${BASH_REMATCH[4]}"
+	[gecos]="${BASH_REMATCH[5]}"
+	[home]="${BASH_REMATCH[6]}"
+	[shell]="${BASH_REMATCH[7]}"
+)
+
+if ! [[ $(getent group "$TARGET_GROUP") =~ ^([^:]+):([^:]*):([^:]*):(.*)$ ]]; then
+	echo "Couldn't parse: getent group" 1>&2
+	exit 1
+fi
+declare -A group=(
+	[name]="${BASH_REMATCH[1]}"
+	[password]="${BASH_REMATCH[2]}"
+	[id]="${BASH_REMATCH[3]}"
+	[users]="${BASH_REMATCH[4]}"
+)
 
 if [[ $MAP_GID && $MAP_GID -ne 0 ]]; then
-	groupmod -g $MAP_GID "$TARGET_GROUP"
+	[[ ${group[id]} -ne $MAP_GID ]] && groupmod -g $MAP_GID "$TARGET_GROUP"
 fi
 if [[ $MAP_UID && $MAP_UID -ne 0 ]]; then
-	usermod -u $MAP_UID "$TARGET_USER" 1>/dev/null
+	[[ ${user[uid]} -ne $MAP_UID ]] && usermod -u $MAP_UID "$TARGET_USER"
 fi
 
-find "$target_home" \
+find "${user[home]}" \
 	'(' -not -user "$TARGET_USER" -a -not -group "$TARGET_GROUP" -exec chown "$TARGET_USER:$TARGET_GROUP" '{}' + ')' -o \
 	'(' -not -user "$TARGET_USER" -exec chown "$TARGET_USER" '{}' + ')' -o \
 	'(' -not -group "$TARGET_GROUP" -exec chgrp "$TARGET_GROUP" '{}' + ')' -o \
